e9440fcd5f43753302fda0fef99f8697169a2bbc
Emilio Jelves <ejjelves@gmail.com>
2021-07-01 13:59:48 -0400

    Version funcionando, menos los Delete que deben correr con .first() para borrar 1 elemenento, siendo que quiero borrar todos

--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ from flask import jsonify
 from flask import request
 from flask import render_template
 from flask_migrate import Migrate
-from models import db, Contact
+from models import db, Persona, Planeta, Usuario
+from models import Personaje_favorito, Planeta_favorito
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -12,93 +13,205 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 Migrate(app, db)
 
+# Main html
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
 
-@app.route('/api/contacts', methods=['GET'])
-def list_contact():
-    contacts = Contact.query.all()
-    contacts = list(map(lambda contact: contact.serialize(), contacts))
-    return jsonify(contacts), 200
+# Primera Parte de Tarea  People, People int, Planets, Planets int
+@app.route('/api/personas', methods=['GET'])
+def list_personas():
+    personas = Persona.query.all()
+    personas = list(map(lambda persona: persona.serialize(), personas))
+    return jsonify(personas), 200
 
 
-@app.route('/api/contacts', methods=['POST'])
-def create_contact():
+@app.route('/api/personas/<int:id>', methods=['GET'])
+def info_persona(id):
+    persona = Persona.query.get(id)
+    return jsonify(persona.serialize()), 200
+
+
+@app.route('/api/planetas', methods=['GET'])
+def list_planetas():
+    planetas = Planeta.query.all()
+    planetas = list(map(lambda planeta: planeta.serialize(), planetas))
+    return jsonify(planetas), 200
+
+
+@app.route('/api/planetas/<int:id>', methods=['GET'])
+def info_planeta(id):
+    planeta = Planeta.query.get(id)
+    return jsonify(planeta.serialize()), 200
+
+
+# Segunda Parte de Tarea: Usuarios, usuarios Favoritos
+# Get, Post y Delete
+@app.route('/api/usuarios', methods=['GET'])
+def list_usuarios():
+    usuarios = Usuario.query.all()
+    usuarios = list(map(lambda usuario: usuario.serialize(), usuarios))
+    return jsonify(usuarios), 200
+
+
+# Retorna Planetas y Personajes Favoritos segun Usuario
+@app.route('/api/usuarios/<int:id>', methods=['GET'])
+def list__favortios_usuarios(id):
+    personajes_favoritos_usuario = Personaje_favorito.query.filter_by(
+        usuario_id=id)
+    planetas_favoritos_usuario = Planeta_favorito.query.filter_by(
+        usuario_id=id)
+
+    personajes_favoritos_usuario = list(
+        map(lambda favorito: favorito.serialize(), personajes_favoritos_usuario))
+    planetas_favoritos_usuario = list(
+        map(lambda favorito: favorito.serialize(), planetas_favoritos_usuario))
+    return jsonify(personajes_favoritos_usuario + planetas_favoritos_usuario), 200
+
+
+# Post a Personaje Favorito según id Usuario
+@app.route('/api/usuarios/personaje/<int:id>', methods=['POST'])
+def create_personaje(id):
     request_body = request.data
     decoded_object = json.loads(request_body)
-    name = decoded_object["name"]
-    phone = decoded_object["phone"]
-    email = decoded_object["email"]
-    contact = Contact()
-    contact.name = name
-    contact.phone = phone
-    contact.email = email
+    usuario_id = id
+    personaje_id = decoded_object["personaje_id"]
 
-    db.session.add(contact)
+    personaje = Personaje_favorito()
+    personaje.usuario_id = usuario_id
+    personaje.personaje_id = personaje_id
+
+    db.session.add(personaje)
     db.session.commit()
 
-    return jsonify(contact.serialize())
+    return jsonify(personaje.serialize())
 
 
-@app.route('/api/contacts/<int:id>', methods=['PUT'])
-def update_contact(id):
+# Post a Planeta Favorito según id Usuario
+@app.route('/api/usuarios/planeta/<int:id>', methods=['POST'])
+def create_planeta_favorito(id):
     request_body = request.data
     decoded_object = json.loads(request_body)
-    name = decoded_object["name"]
-    phone = decoded_object["phone"]
-    email = decoded_object["email"]
+    usuario_id = id
+    planeta_id = decoded_object["planeta_id"]
+    planeta = Planeta_favorito()
+    planeta.usuario_id = usuario_id
+    planeta.planeta_id = planeta_id
 
-    contact = Contact.query.get(id)
-    contact.name = name
-    contact.phone = phone
-    contact.email = email
-
+    db.session.add(planeta)
     db.session.commit()
 
-    return jsonify(contact.serialize()), 200
+    return jsonify(planeta.serialize())
 
 
-@app.route('/api/contacts/<int:id>', methods=['DELETE'])
-def delete_contact(id):
+# Delete
+@app.route('/api/usuarios/personaje/<int:id>', methods=['DELETE'])
+def delete_personaje(id):
 
-    contact = Contact.query.get(id)
-    db.session.delete(contact)
+    personajes_favoritos_usuario = Personaje_favorito.query.filter(
+        Personaje_favorito.usuario_id == id).first()
+    db.session.delete(personajes_favoritos_usuario)
     db.session.commit()
 
-    return jsonify({"success": "Contact deleted"}), 200
+    return jsonify({"success": "Personaje Favorito deleted"}), 200
+
+
+@app.route('/api/usuarios/planeta/<int:id>', methods=['DELETE'])
+def delete_planeta(id):
+    planetas_favoritos_usuario = Planeta_favorito.query.filter(
+        Planeta_favorito.usuario_id == id)
+    db.session.delete(planetas_favoritos_usuario)
+    db.session.commit()
+
+    return jsonify({"success": "Planeta Favorito deleted"}), 200
+
+
+# Solo para Ingresar Personas/Planetas a BD y Usuarios
+@app.route('/api/personas', methods=['POST'])
+def create_persona():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    nombre = decoded_object["nombre"]
+    altura = decoded_object["altura"]
+    masa = decoded_object["masa"]
+    descripcion = decoded_object["descripcion"]
+    persona = Persona()
+    persona.nombre = nombre
+    persona.altura = altura
+    persona.masa = masa
+    persona.descripcion = descripcion
+
+    db.session.add(persona)
+    db.session.commit()
+
+    return jsonify(persona.serialize())
+
+
+@app.route('/api/planetas', methods=['POST'])
+def create_planeta():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    nombre = decoded_object["nombre"]
+    diametro = decoded_object["diametro"]
+    poblacion = decoded_object["poblacion"]
+    descripcion = decoded_object["descripcion"]
+    planeta = Planeta()
+    planeta.nombre = nombre
+    planeta.diametro = diametro
+    planeta.poblacion = poblacion
+    planeta.descripcion = descripcion
+
+    db.session.add(planeta)
+    db.session.commit()
+
+    return jsonify(planeta.serialize())
+
+
+@app.route('/api/usuarios', methods=['POST'])
+def create_usuario():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    correo = decoded_object["correo"]
+    clave = decoded_object["clave"]
+    usuario = Usuario()
+    usuario.correo = correo
+    usuario.clave = clave
+
+    db.session.add(usuario)
+    db.session.commit()
+
+    return jsonify(usuario.serialize())
+
+# @app.route('/api/people/<int:id>', methods=['PUT'])
+# def update_persona(id):
+#     request_body = request.data
+#     decoded_object = json.loads(request_body)
+#     name = decoded_object["name"]
+#     phone = decoded_object["phone"]
+#     email = decoded_object["email"]
+
+#     persona = Persona.query.get(id)
+#     persona.name = name
+#     persona.phone = phone
+#     persona.email = email
+
+#     db.session.commit()
+
+#     return jsonify(persona.serialize()), 200
+
+
+# @app.route('/api/people/<int:id>', methods=['DELETE'])
+# def delete_persona(id):
+
+#     persona = Persona.query.get(id)
+#     db.session.delete(persona)
+#     db.session.commit()
+
+#     return jsonify({"success": "persona deleted"}), 200
 
 
 if __name__ == '__main__':
     app.run()
-
-# @app.route('/todos', methods=['GET'])
-# def hello_world():
-#     return jsonify(todos)
-
-# todos = [
-#     {"label": "My first task", "done": False},
-#     {"label": "My second task", "done": False}
-# ]
-
-# @app.route('/todos', methods=['POST'])
-# def add_new_todo():
-#     request_body = request.data
-#     decoded_object = json.loads(request_body)
-#     # print("Incoming request with the following body", request_body)
-#     todos.append(decoded_object)
-#     return jsonify(todos)
-
-
-# @app.route('/todos/<int:position>', methods=['DELETE'])
-# def delete_todo(position):
-#     todos.pop(position)
-#     # print("This is the position to delete: ", position)
-#     return jsonify(todos)
-
-
-# # These two lines should always be at the end of your app.py file.
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=3245, debug=True)
--- src/models.py
+++ src/models.py
@@ -2,17 +2,83 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Contact(db.Model):
-    __tablename__ = 'contacts'
+class Persona(db.Model):
+    __tablename__ = 'personas'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    altura = db.Column(db.String(100), nullable=False)
+    masa = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(100), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
-            "phone": self.phone,
-            "email": self.email
+            "nombre": self.nombre,
+            "altura": self.altura,
+            "masa": self.masa,
+            "descripcion": self.descripcion
+        }
+
+
+class Planeta(db.Model):
+    __tablename__ = 'planetas'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    diametro = db.Column(db.String(100), nullable=False)
+    poblacion = db.Column(db.String(100), nullable=False)
+    descripcion = db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "diametro": self.diametro,
+            "poblacion": self.poblacion,
+            "descripcion": self.descripcion
+        }
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    correo = db.Column(db.String(100), nullable=False, unique=True)
+    clave = db.Column(db.String(20), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "correo": self.correo,
+            "clave": self.clave
+        }
+
+
+class Personaje_favorito(db.Model):
+    __tablename__ = 'personajes_favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey(
+        "usuarios.id"), nullable=False)
+    personaje_id = db.Column(db.Integer, db.ForeignKey(
+        "personas.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "personaje_id": self.personaje_id
+        }
+
+
+class Planeta_favorito(db.Model):
+    __tablename__ = 'plentas_favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey(
+        "usuarios.id"), nullable=False)
+    planeta_id = db.Column(db.Integer, db.ForeignKey(
+        "planetas.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "planeta_id": self.planeta_id
         }
